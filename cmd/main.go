f7c64ac3219bc556d97d635201f0eaacc9a8f15a
Gabe Timm <gabe.m.timm@gmail.com>
2024-10-26 11:41:41 -0700

    add export when graph is completed

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
+	"movie-graph/internal/graph"
 	"movie-graph/internal/importer"
 )
 
 func main() {
-	// graph, err := graph.ImportGraph("./export")
+	// movieGraph, err := graph.ImportGraph("./export")
 	// if err != nil {
 	// 	fmt.Println("Error importing graph:", err)
 	// 	return
 	// }
-	graph := importer.GenerateGraph()
+	movieGraph := importer.GenerateGraph()
 
-	if graph != nil {
+	graph.ExportGraph(movieGraph, "./export")
+
+	if movieGraph != nil {
 		fmt.Println("Graph created successfully")
 		// fmt.Printf("Index: %v\n", graph.Index)
 		// fmt.Printf("Edges: %v\n", graph.Edges)
